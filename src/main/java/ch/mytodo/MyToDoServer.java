6f0a32d5661211ecac39ef5ff71727d6ae055f8a
degob <alvaro_cp81@yahoo.es>
2017-08-31 10:30:10 +0200

    bind static html page

--- src/main/java/ch/mytodo/MyToDoServer.java
+++ src/main/java/ch/mytodo/MyToDoServer.java
@@ -2,8 +2,15 @@ package ch.mytodo;
 
 import ch.mytodo.api.DebugService;
 import ch.mytodo.impl.PersistenceServiceImpl;
+import com.google.common.collect.Maps;
+import com.google.common.io.CharStreams;
+import spark.*;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import static spark.Spark.get;
+import static spark.Spark.modelAndView;
 
 public class MyToDoServer {
 
@@ -20,6 +27,22 @@ public class MyToDoServer {
             response.type("application/json");
             return debugService.schemaVersionInfo();
         });
+        TemplateViewRoute route = (request, response) ->
+                modelAndView(Maps.newHashMap(), "index.html");
+        get("/mytodo-app", route, new HtmlEngine());
     }
 
+    private class HtmlEngine extends TemplateEngine {
+        @Override
+        public String render(ModelAndView modelAndView) {
+            InputStream stream = HtmlEngine.class.getResourceAsStream("/" + modelAndView.getViewName());
+            String html = "";
+            try {
+                html = CharStreams.toString(new InputStreamReader(stream, "UTF-8"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return html;
+        }
+    }
 }
